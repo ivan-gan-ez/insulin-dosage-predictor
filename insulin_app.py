6abae1a5ccfb96cf29184cf3d409b4e9ff5e05de
ivan-gan-ez <ivan.ee.zern.gan@gmail.com>
2025-12-31 00:00:59 +0800

    more app stuff

--- insulin_app.py
+++ insulin_app.py
@@ -10,18 +10,18 @@ from sklearn.compose import ColumnTransformer
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.model_selection import train_test_split
 
-st.set_page_config(layout='wide', page_title='Diabetes ML App')
+st.set_page_config(layout='wide', page_title='Insulin Dosage Predictor App', page_icon="IDPA logo.svg")
 
 ## data loading
 @st.cache_data
 def load_data():
-    URL = 'insulin_dosage_prediction.csv'
+    URL = './insulin_dosage_prediction.csv'
     df = pd.read_csv(URL)
     return df
 
 df = load_data()
 
-model = joblib.load('insulin_dosage_predictor.joblib')
+model = joblib.load('./insulin_dosage_predictor.joblib')
 
 @st.cache_resource
 def train_model(data):
@@ -59,8 +59,28 @@ if page == 'Predictor':
     st.success(f'Model Accuracy: {(accuracy*100):.4f}%')
 elif page == 'Data':
     st.title("A Glance At The Dataset")
+    st.markdown('Explore the dataset.')
+    st.divider()
+    
+    tab1, tab2, tab3 = st.tabs(['Overview', 'Distributions', 'Raw Data'])
+    
+    with tab1: 
+        fig1 = px.histogram(df, x='BMI', y='glucose_level', color='Insulin', hover_data=['BMI', 'glucose_level'], title='BMI vs Glucose')
+        st.plotly_chart(fig1, use_container_width=True)
 
+        c1_1, c1_2 = st.columns([2/5, 3/5])
+        with c1_1: 
+            fig2 = px.histogram(df, x='family_history', y='glucose_level', color='Insulin', hover_data=['family_history', 'glucose_level'], title='Genetics vs Glucose')
+            st.plotly_chart(fig2, use_container_width=True)
+        with c1_2: 
+            fig3 = px.histogram(df, x='food_intake', y='glucose_level', color='Insulin', hover_data=['food_intake', 'glucose_level'], title='Food Intake vs Glucose')
+            st.plotly_chart(fig3, use_container_width=True)
 
+        fig4 = px.histogram(df, x='insulin_sensitivity', y='glucose_level', color='Insulin', hover_data=['insulin_sensitivity', 'glucose_level'], title='Insulin Sensitivity vs Glucose')
+        st.plotly_chart(fig4, use_container_width=True)
+
+        fig5 = px.histogram(df, x='weight', y='glucose_level', color='Insulin', hover_data=['weight', 'glucose_level'], title='Weight vs Glucose')
+        st.plotly_chart(fig5, use_container_width=True)
 
 
 
